e3e6d34f0fdf64f97a055921fa41bd1ca0aec4f5
hanbin <han_bin@foxmail.com>
2019-01-28 02:49:55 +0800

    添加配置中心测试代码

---
+++ customer/src/main/java/cn/com/hanbinit/customer/controller/UserController.java
@@ -0,0 +1,22 @@
+package cn.com.hanbinit.customer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class UserController {
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private Long age;
+
+    @GetMapping("/showinfo")
+    public String showNameAndAge(){
+        return "name: " + name + ", age: " + age;
+    }
+}
